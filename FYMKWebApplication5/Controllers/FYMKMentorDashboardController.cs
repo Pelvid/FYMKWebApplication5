74599b67d869c6164518b4e1c90178a92557cabe
davidgiwa8 <davidgiwa8@gmail.com>
2022-02-22 13:57:01 -0800

    FYMKWebApplication5

---
+++ FYMKWebApplication5/Controllers/FYMKMentorDashboardController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FYMKWebApplication5.Controllers
+{
+    public class FYMKMentorDashboardController : Controller
+    {
+        // GET: FYMKMentor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Mentee()
+        {
+            return View();
+        }
+        public ActionResult MentorAlert()
+        {
+            return View();
+        }
+        public ActionResult MentorCalender()
+        {
+            return View();
+        }
+        public ActionResult MentorDashboard()
+        {
+            return View();
+        }
+        public ActionResult MentorProfile()
+        {
+            return View();
+        }
+        public ActionResult MentoreProgram()
+        {
+            return View();
+        }
+        public ActionResult Chat()
+        {
+            return View();
+        }
+        public ActionResult Setting()
+        {
+            return View();
+        }
+
+        public ActionResult HelpCentre()
+        {
+            return View();
+        }
+    }
+}
