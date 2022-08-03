b681389951b3fd581674b8cdcc48d8d7816570e3
davidgiwa8 <davidgiwa8@gmail.com>
2022-08-02 17:05:56 -0700

    update

---
+++ FYMKWebApplication5/Models/FeedBack.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FYMKWebApplication5.Models
+{
+    public class FeedBack
+    {
+        public string Feedback { get; set; }
+    }
+}
